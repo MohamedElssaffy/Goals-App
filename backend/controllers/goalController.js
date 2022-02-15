c63f668b7d1a6c3dbd618d2f73915eb27bb29fa2
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2022-02-16 01:26:24 +0200

    init commit

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,65 @@
+const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel');
+
+// GET /api/goals
+// Private
+
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find();
+  res.json(goals);
+});
+
+// POST /api/goals
+// Private
+
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text');
+  }
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(201).json(goal);
+});
+
+// PUT /api/goals/id
+// Private
+
+const updateGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(404);
+    throw new Error('Goal not found');
+  }
+
+  goal.text = req.body.text;
+  await goal.save();
+  res.json(goal);
+});
+
+// DELETE /api/goals/id
+// Private
+
+const deleteGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(404);
+    throw new Error('Goal not found');
+  }
+
+  await goal.remove();
+  res.json({ id: req.params.id });
+});
+
+module.exports = {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+};
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const colors = require('colors');
+const { errorHandler } = require('./middlewares/errorMiddleware');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const goalRoute = require('./routes/goalRoute');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', goalRoute);
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Serever is running on port ${port}`));
